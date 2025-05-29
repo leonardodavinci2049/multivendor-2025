a33c693b1cb7888a0784a7968f43bd54a42ce9ab
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-05-29 09:29:52 -0300

    feat: adicionar botão de usuário do Clerk na página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import ThemeToggle from "@/components/ui/shared/ThemeToggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-5">
       <div className=" flex justify-end">
+        <UserButton />
         <ThemeToggle />
       </div>
       <h1 className="font-barlow text-3xl font-bold text-red-500">
